77e7d87d8bfb6899e74e25b68e913f0a360c6622
Hoshyar Karimi <hoshyarkarimi@outlook.com>
2022-08-27 23:19:01 +0430

    matplotlib

--- Learning/numpy and matplotlib/matplotlib_learning.py
+++ Learning/numpy and matplotlib/matplotlib_learning.py
@@ -15,11 +15,29 @@ import numpy as np
 # print(data.transpose())
 # plt.show()
 
-days = np.arange(0, 21)
-other_site = np.arange(0, 21)
-real_python = other_site ** 2
+# days = np.arange(0, 21)
+# other_site = np.arange(0, 21)
+# real_python = other_site ** 2
 
-plt.plot(days, other_site)
-plt.plot(days, real_python)
-plt.xticks([0, 5, 10, 15, 20])
+# plt.plot(days, other_site)
+# plt.plot(days, real_python)
+# plt.xticks([0, 5, 10, 15, 20])
+# plt.xlabel("Days of Reading")
+# plt.ylabel("Amount of Python Learned")
+# plt.title("Python Learned Reading Real Python vs Other Site")
+# plt.legend(["Other Site", "Real Python"], loc="best")
+# plt.show()
+
+# xs = np.arange(1, 6)
+# tops = np.arange(2, 11, 2)
+# plt.bar(xs, tops)
+# plt.show()
+
+fruits = {
+    "apples": 5,
+    "oranges": 10,
+    "cucumbers": 3,
+    "pears": 9
+}
+plt.bar(fruits.keys(), fruits.values())
 plt.show()
